7de69f39884af3743af4ede2d2f3a4b674ccd72d
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2018-02-16 11:58:55 +0300

    Enabling initial Netwok Connection.
    -ToDo
    Rework the interface first

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    ServerMainWindow w;
+
     w.show();
+
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
 
     return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,20 +3,68 @@
 
 #include <QMainWindow>
 
+
+#include <QObject>
+#include <QWidget>
+#include <QDialog>
+#include <QLayout>
+#include <QtWidgets>
+#include <QtNetwork>
+#include <QString>
+#include <stdlib.h>
+#include <QNetworkSession>
+#include <QTcpServer>
+
+typedef std::pair <QTcpSocket*, QString> namedConnection;
+typedef std::pair <QDataStream*, int> identifiedStreams;
+
+
+
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class ServerMainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit ServerMainWindow(QWidget *parent = 0);
+    ~ServerMainWindow();
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+
+    void sessionOpened();
+    void sendFortune(int socketDescriptor);
+
+    void setUpNewConnection();
+    void readTheClientName(int socketDescriptor);
+    void displayError(QAbstractSocket::SocketError);
+
+
+signals:
+
+    void signal_sendFortune(int socketDescriptor);
+
+private:
+
+    QLabel *statusLabel;
+    QVBoxLayout * _mainLayout;
+    std::vector<QLabel*> _connectedClients;
+
+    QTcpServer *tcpServer;
+    QStringList fortunes;
+    QNetworkSession *networkSession;
+
+    std::vector<namedConnection> _establishedConnections;
+    std::vector<identifiedStreams> _dataStreams;
+
+    int _currentBuggedSocket = 0;
+
+
 };
 
 #endif // MAINWINDOW_H
